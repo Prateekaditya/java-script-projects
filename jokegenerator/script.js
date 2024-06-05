52d6b9f733a5f43268046a6ba90172ca79d17f4f
Prateek Aditya <prateek0aditya@gmail.com>
2024-06-05 15:06:37 +0530

    intial commit

---
+++ jokegenerator/script.js
@@ -0,0 +1,15 @@
+let jokeContainer=document.getElementById("joke");
+let btn=document.getElementById("btn");
+const url= "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
+
+let getjoke = () =>{
+    jokeContainer.classList.remove("fade");
+    fetch(url)
+    .then(data=>data.json())
+    .then(item=>{
+        jokeContainer.textContent= `${item.joke}`;
+        jokeContainer.classList.add("fade");
+    });
+}
+btn.addEventListener("click",getjoke);
+// getjoke();
---
+++ responsice counting/script.js
@@ -0,0 +1,16 @@
+let valueDisplays=document.querySelectorAll(".num");
+let interval=4000;
+
+// console.log(valueDisplay);
+valueDisplays.forEach((valueDisplay) =>{
+    let startvalue=0;
+    let endvalue=parseInt(valueDisplay.getAttribute("data-val"));
+    let duration=Math.floor(interval/endvalue);
+    let counter= setInterval( function () {
+        startvalue += 1;
+        valueDisplay.textContent= startvalue;
+        if(startvalue===endvalue){
+            clearInterval(counter);
+        }
+    }, duration) ;
+});
